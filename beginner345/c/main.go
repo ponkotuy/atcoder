023274acca0f0f044c52be78c7afb1bb1be7ce59
ponkotuy <web@ponkotuy.com>
2024-03-19 01:01:52 +0900

    Speedup Begginer345/c

--- beginner345/c/main.go
+++ beginner345/c/main.go
@@ -1,15 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func circular2(x int) int {
 	return x * (x - 1) / 2
 }
 
 func core(input string) int {
-	cMap := make(map[rune]int)
+	cMap := make([]int, 26)
 	for _, c := range input {
-		cMap[c] += 1
+		cMap[c-'a'] += 1
 	}
 	dup := 0
 	for _, value := range cMap {
@@ -25,6 +29,7 @@ func core(input string) int {
 
 func main() {
 	var input string
-	fmt.Scan(&input)
+	rdr := bufio.NewReaderSize(os.Stdin, 1e6+10)
+	fmt.Fscan(rdr, &input)
 	fmt.Println(core(input))
 }
